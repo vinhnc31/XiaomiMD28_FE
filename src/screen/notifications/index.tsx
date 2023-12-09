5bfe205226ad54c5a30d4d7d15f45787154d384f
phuclhdrake <phucleaaee@gmail.com>
2023-12-09 07:15:29 +0700

    add SCREEN notification & view cart màn list product

--- src/screen/notifications/index.tsx
+++ src/screen/notifications/index.tsx
@@ -1,17 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
-import { Text, SafeAreaView, Button, View, StyleSheet, TouchableOpacity, Dimensions, FlatList, Image, Modal, TextInput } from 'react-native';
-import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
-import { CompositeNavigationProp, RouteProp } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { AppStackParam, MenuStackParam } from '@src/navigations/AppNavigation/stackParam';
-import { MENU_NAVIGATION } from '@src/navigations/routes';
-import { BottomPopup } from '../../containers/components/Base/BottomPopup'
-import { CategoryModel } from '@src/services/category/category.model';
-import CategoryService from '@src/services/category';
-
-import { ProductModel } from '@src/services/product/product.model';
-import ProductService from '@src/services/product';
+import {
+  Text,
+  SafeAreaView,
+  Button,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  FlatList,
+  Image,
+  ScrollView,
+  Modal,
+  TextInput,
+  RefreshControl,
+} from 'react-native';
+import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
+import {CompositeNavigationProp, RouteProp} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {AppStackParam, MenuStackParam} from '@src/navigations/AppNavigation/stackParam';
+import {MENU_NAVIGATION, APP_NAVIGATION, GUEST_NAVIGATION} from '@src/navigations/routes';
+import {navigateToPage, goBack} from '@src/navigations/services';
+import {NotificationModel} from '@src/services/notification/notification.model';
+import {useAuth} from '@src/hooks/useAuth';
+import NotificationService from '@src/services/notification';
+import TouchableScale from 'react-native-touchable-scale';
+import styles from './styles';
+import {BaseLoading} from '@src/containers/components/Base';
+import {ms, vs, hs} from '@src/styles/scalingUtils';
 
 export type ScreenNavigationProps = CompositeNavigationProp<
   BottomTabNavigationProp<MenuStackParam, MENU_NAVIGATION.NOTIFICATIONS>,
@@ -23,17 +39,115 @@ interface Props {
   route: RouteProp<MenuStackParam, MENU_NAVIGATION.NOTIFICATIONS>;
 }
 
-
-
 const NotificationScreen = (props: Props) => {
+  const [data, setData] = useState<NotificationModel[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const [error, setError] = useState('');
+  const {user} = useAuth();
+  const AccountId = user?.id;
 
- 
+  const handleBackPress = () => {
+    goBack();
+  };
+
+  useEffect(() => {
+    fetchDataNotification();
+    console.log('eff: ', data);
+  }, []);
+
+  const fetchDataNotification = async () => {
+    try {
+      setLoading(true);
+      const notificationService = new NotificationService();
+      const result = await notificationService.getNotification(AccountId);
+      // console.log(result.data);
+      setData(result.data);
+      setLoading(false);
+    } catch (error) {
+      setError('err');
+      setLoading(false);
+    }
+  };
+
+  const onRefresh = async () => {
+    await fetchDataNotification();
+    setLoading(false);
+  };
+  const renderNoDataMessage = () => {
+    if (loading) {
+      return (
+        <View style={styles.LoadingContainer}>
+          <BaseLoading size={60} top={250} loading={true} />
+        </View>
+      );
+    }
+
+    if (data.length === 0) {
+      return (
+        <View style={styles.noDataContainer}>
+          <Image style={{width: hs(120), height: hs(120)}} source={require('../../assets/images/noDataStar.png')} />
+          <Text style={styles.noDataText}>Không có thông báo</Text>
+        </View>
+      );
+    }
+
+    return null;
+  };
+
   return (
+    <SafeAreaView style={{flex: 1, flexDirection: 'column', backgroundColor: '#F8F8F8'}}>
+      <View>
+        <View style={styles.headerContainer}>
+          <View style={{flex: 8, alignItems: 'flex-start', justifyContent: 'center'}}>
+            <Text numberOfLines={1} style={styles.headerTitle}>
+              Thông báo
+            </Text>
+          </View>
+        </View>
+        <View style={{borderColor: '#A7A7A7', borderWidth: 0.8}} />
+      </View>
 
-    <View style={{ flex: 1, justifyContent: 'space-around' }}>
-      
-    </View>
+      <ScrollView
+        indicatorStyle="black"
+        showsVerticalScrollIndicator={false}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+        {renderNoDataMessage()}
+        {data.length > 0 && (
+          <View style={{flex: 1,}}>
+            <FlatList
+              data={data}
+              // keyExtractor={(item) => item.id}
+              showsVerticalScrollIndicator={false}
+              renderItem={({item, index}) => (
+                <>
+                  <TouchableScale
+                  key={index}
+                  onPress={() => console.log('click---', item.id)}
+                  activeScale={1}
+                  friction={9}
+                  tension={100}
+                  style={{height: vs(125), justifyContent: 'center', alignItems: 'center'}}
+                  >
+                  <View style={styles.viewItemNotification}>
+                    <View style={{flex: 2}}>
+                      {/* {item.image ? <Image source={{uri: item.image}} style={styles.imgNotification} /> : <Text>No Image</Text>} */}
+                      {/* {item.image ? <Image source={{uri: item.title}} style={styles.imgNotification} /> : <Image source={require('../../assets/images/demo.jpg')} style={styles.imgNotification} />} */}
+                      <Image source={require('../../assets/images/demo.jpg')} style={styles.imgNotification} />
+                    </View>
 
+                    <View style={styles.viewTextNotification}>
+                      <Text numberOfLines={4} style={styles.textContentNotification}>{item?.content}</Text>
+                    </View>
+                  </View>
+                </TouchableScale>
+                </>
+              )}
+            />
+          </View>
+        )}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
---
+++ src/services/notification/notification.model.tsx
@@ -0,0 +1,8 @@
+export interface NotificationModel {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  AccountId: number;
+}
